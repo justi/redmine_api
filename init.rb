6eb7d49bab192daed9a9293c324eb1ae48c7f39e
Justyna Wojtczak <justine84@gmail.com>
2015-10-06 15:45:02 +0200

    Bump version to 0.0.2

--- init.rb
+++ init.rb
@@ -4,5 +4,5 @@ Redmine::Plugin.register :redmine_api do
   name 'Redmine Api plugin'
   author 'Justyna Wojtczak'
   description 'A plugin for providing api'
-  version '0.0.1'
+  version '0.0.2'
 end
